8423d1b38c6612b687a06bdea3012504c7537b91
hanter <mohamed.elhaidi.1997@gmail.com>
2022-07-23 16:08:18 +0100

    added my portrait

--- src/js/data/profile.js
+++ src/js/data/profile.js
@@ -1,3 +1,4 @@
+import portraitIMG from "../../assets/profile/portrait.png";
 export const profile = {
   firstName: "Mohamed",
   secondName: "El-haidi",
@@ -5,8 +6,7 @@ export const profile = {
   bio: `Hi, my name is Mohamed El-haidi from Morocco, web developer. started to learn frontend web developent 5 months ago by learning HTML and CSS/SCSS first, then moved to JavaScript and recently ReactJs.\n
           My ambition is to become a Fullstack web developer in the near future by learning even more technologies.\n
           I choosed to learn Web dev because it's so amazing to be able to create different kind of apps. The challenges I face during this process of learning makes my passion for this grow more and more`,
-  imageProfileURL:
-    "https://media.istockphoto.com/photos/portrait-of-a-smart-and-handsome-it-specialist-wearing-glasses-smiles-picture-id1197071216?k=20&m=1197071216&s=612x612&w=0&h=JEx2IgXxCr1ZNrgRLzbwx55CjpDxiDU0Vg8_ifjR6Pc=",
+  imageProfileURL: portraitIMG,
   frontEndSkills: {
     name: "Front-end",
     tags: [
