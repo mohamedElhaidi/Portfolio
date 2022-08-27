5fc54ea4e5da09551ce08b7f3284067f521f3ee8
hanter <mohamed.elhaidi.1997@gmail.com>
2022-08-27 08:14:33 +0100

    v5

--- src/js/data/profile.js
+++ src/js/data/profile.js
@@ -2,9 +2,9 @@ import portraitIMG from "../../assets/profile/portrait.png";
 export const profile = {
   firstName: "Mohamed",
   secondName: "El-haidi",
-  dateOfBirth: "1997-03-03",
-  bio: `Hi, my name is Mohamed El-haidi from Morocco,A self-taught web developer. Coding is my love of life since I was little and I always had that vision of making amazing Apps. I started learing to code by myself by watching and reading online courses and I feel that I have enough tech knowledge to begin my career.\n
-          My ambition is to become a Fullstack web developer in the near future by learning even more technologies from web to mobile and others.\n
+  dateOfBirth: "1997",
+  bio: `Hi, my name is Mohamed El-haidi from Morocco,A self-taught web developer. Coding is my love of life since I was little and always had a dream of making amazing Apps. I started learing to code by watching and reading online courses.\n
+          My ambition is to become a Fullstack web developer in the near future by learning even more technologies.\n
           `,
   imageProfileURL: portraitIMG,
   frontEndSkills: {
